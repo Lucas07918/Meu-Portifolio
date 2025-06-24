bfe444e20c059dc113e9a6baf96da87eb0d6aac0
Lucas Silva <lucassilva07918@gmail.com>
2025-06-24 16:23:28 -0300

    Pequenas Alterações nos projetos

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -9,12 +9,12 @@ const projects = [
       title: "Furia Know Your Fan",
       description: "Web platform developed to bring FURIA fans closer to the organization. It allows users to share their interests and social media to personalize their experience, receiving content and recommendations tailored to their profile.",
       technologies: ["React", "Firebase", "AWS"],
-      cover: "/public/assets/img/furiaCapa.png",
+      cover: "/assets/img/furiaCapa.png",
       link: "https://know-your-fan.vercel.app/",
       gallery: [
-        "/public/assets/img/FuriaKYFPrint.PNG",
-        "/public/assets/img/FuriaKYFPrint2.PNG",
-        "/public/assets/img/Hubpage_Furia.png",
+        "/assets/img/FuriaKYFPrint.PNG",
+        "/assets/img/FuriaKYFPrint2.PNG",
+        "/assets/img/Hubpage_Furia.png",
       ]
     },
     {
@@ -22,11 +22,11 @@ const projects = [
       title: "Slow Games",
       description: "News portal about video games and pop culture, inspired by the FlowGames.gg website.",
       technologies: ["HTML/CSS", "Javascript"],
-      cover: "/public/assets/img/SlowGames.png",
+      cover: "/assets/img/SlowGames.png",
       link: "https://lucas07918.github.io/Slow-Games/",
       gallery: [
-        "/public/assets/img/SlowGamesHome.PNG",
-        "/public/assets/img/SlowGamesPrint.PNG"
+        "/assets/img/SlowGamesHome.PNG",
+        "/assets/img/SlowGamesPrint.PNG"
       ]
     },
     {
@@ -34,13 +34,13 @@ const projects = [
       title: "Diet Generator",
       description: "A personalized diet generator website that allows you to create a custom meal plan with just one click.",
       technologies: ["Python", "Flask",],
-      cover: "/public/assets/img/DietGeneratorCapa.png",
+      cover: "/assets/img/DietGeneratorCapa.png",
       link: "https://github.com/Lucas07918/Gerador-de-Dieta---Projeto",
       gallery: [
-        "/public/assets/img/geradorDieta-white.PNG",
-        "/public/assets/img/geradorDieta-dark.PNG",
-        "/public/assets/img/Print gerador de dieta.PNG",
-        "/public/assets/img/geradorDieta-dark2.PNG"
+        "/assets/img/geradorDieta-white.PNG",
+        "assets/img/geradorDieta-dark.PNG",
+        "/assets/img/Print gerador de dieta.PNG",
+        "/assets/img/geradorDieta-dark2.PNG"
       ]
     },
   ];
